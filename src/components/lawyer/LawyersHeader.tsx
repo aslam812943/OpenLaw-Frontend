c32f207ee9151c85b36a3be338008e14109456c3
Aslam <aslam812943@gmai.com>
2026-02-05 20:44:39 +0530

    fix(ui) small problem

--- src/components/lawyer/LawyersHeader.tsx
+++ src/components/lawyer/LawyersHeader.tsx
@@ -88,7 +88,7 @@ const Header: React.FC<HeaderProps> = ({
             >
               <Bell className="w-5 h-5 group-hover:text-teal-600" />
               {unreadCount > 0 && (
-                <span className="absolute top-2 right-2 w-4 h-4 bg-red-500 text-white text-[10px] font-black flex items-center justify-center rounded-full ring-2 ring-white">
+                <span className="absolute top-1 right-1 w-4 h-4 bg-red-500 text-white text-[10px] font-black flex items-center justify-center rounded-full ring-2 ring-white z-50">
                   {unreadCount}
                 </span>
               )}
